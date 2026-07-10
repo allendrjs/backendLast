d889215984cf35f0a35045ab6544eb929422c3e0
Keith Jasper Quevada <keithjasper.quevada@gmail.com>
2026-07-10 12:39:34 +0800

    OSDA-518: Scaffolding

---
+++ src/main/java/org/rocs/osdrmsa/OfficeOfStudentDisciplineWebApplication.java
@@ -0,0 +1,14 @@
+package org.rocs.osdrmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeOfStudentDisciplineWebApplication {
+
+	public static void cdmain(String[] args) {
+		SpringApplication.run(OfficeOfStudentDisciplineWebApplication.class, args);
+
+	}
+
+}
